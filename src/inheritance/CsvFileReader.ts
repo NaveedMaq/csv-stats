aaf0988ece5664f986ccc911618f941274af4d34
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-10-04 12:09:05 +0530

    refactor to object composition

--- src/inheritance/CsvFileReader.ts
+++ src/inheritance/CsvFileReader.ts
@@ -1,10 +1,11 @@
 import fs from 'fs';
-import { DataReader } from './DataReader';
 
-export class CsvFileReader implements DataReader {
-  data: string[][] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
@@ -12,6 +13,7 @@ export class CsvFileReader implements DataReader {
         encoding: 'utf-8',
       })
       .split('\n')
-      .map((row: string): string[] => row.split(','));
+      .map((row: string): string[] => row.split(','))
+      .map(this.mapRow);
   }
 }
